a9128328ddb8f7b04ec578ba76a3521e5823a1f5
modnikhil <mod.nikhil@gmail.com>
2019-02-19 06:43:06 +0100

    Feat #18 Created AssignmentsTest Skeleton

---
+++ bt-tests/src/test/java/bt/torrent/messaging/AssignmentsTest.java
@@ -0,0 +1,35 @@
+package bt.torrent.messaging;
+
+import org.junit.Test;
+
+import bt.net.Peer;
+import bt.protocol.Have;
+import bt.protocol.Message;
+import bt.protocol.Piece;
+import bt.torrent.annotation.Consumes;
+import bt.torrent.annotation.Produces;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class AssignmentsTest {
+
+    @Test
+    public void assign() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
